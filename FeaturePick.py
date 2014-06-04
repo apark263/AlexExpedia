87032f1f1d0a516b2816feee830ed9e240aca4c2
apark1979 <alex.park@gmail.com>
2014-06-03 22:59:45 -0700

    Re-wrote a bunch of the functions and added probability of child friendly property feature Also changed way train/dev/test sets are partitioned

--- FeaturePick.py
+++ FeaturePick.py
@@ -9,59 +9,78 @@ import cPickle as pickle
 from subprocess import call
 import sys, getopt
 
-def calc_prop_features(train, test, recalc=0):
+def get_avg_vector(indices, event):
+	sumcounts = np.bincount(indices.astype(int), event)
+	sumtotals = np.bincount(indices.astype(int))
+	return sumcounts/sumtotals
+
+def get_cond_prob_vector(indices, event1, event2):
+	sumboth = np.bincount(indices.astype(int), event1*event2)
+	sumsecond = np.bincount(indices.astype(int), event2)
+	condprob = sumboth
+	condprob[sumsecond!=0] = condprob[sumsecond!=0]/sumsecond[sumsecond!=0]
+	return condprob
+
+def calc_prop_features(train, dev, test, recalc=0):
 	if (recalc == 0):
 		try:
 			in_path = data_io.get_paths()["features_cache"]
 			rfp = open(in_path, "r")
 			px_avg    = pickle.load(rfp)
-			clck_prob = pickle.load(rfp)
-			book_prob = pickle.load(rfp)
-			clck_prob_all = pickle.load(rfp)
-			book_prob_all = pickle.load(rfp)
+			clck_prob, book_prob, cf_prob = pickle.load(rfp)
+			clck_prob_all, book_prob_all, cf_prob_all = pickle.load(rfp)
 		except:
 			recalc = 1	    	
 	if (recalc == 1):
-		# Average Price
-		tlist = pd.concat((train[['prop_id','price_usd']], test[['prop_id','price_usd']]))
-		px_sum = np.bincount(tlist['prop_id'].values, tlist['price_usd'].values)
-		px_cnt = np.bincount(tlist['prop_id'].values)
-		px_avg = px_sum/px_cnt
+		# Average Price	
+		prop_px_keys = ['prop_id', 'price_usd']	
+		pxr = np.concatenate((train[prop_px_keys].values, dev[prop_px_keys].values, test[prop_px_keys].values))
+		px_avg = get_avg_vector(pxr[:,0], pxr[:,1])
 
-		# Click and Book Probs
-		train_nodev = train[(train['srch_id'] % 10 != 1)][['prop_id', 'booking_bool', 'click_bool']]
-		book_sum = np.bincount(train_nodev['prop_id'].values, train_nodev['booking_bool'].values)	
-		clck_sum = np.bincount(train_nodev['prop_id'].values, train_nodev['click_bool'].values)
-		clck_cnt = np.bincount(train_nodev['prop_id'].values)
-		clck_prob = clck_sum/clck_cnt
-		book_prob = book_sum/clck_cnt
+		# Click and Book Probs for just train
+		prop_keys = ['prop_id', 'booking_bool', 'click_bool', 'srch_children_count']
+		pbc = train[prop_keys].values
+		clck_prob = get_avg_vector(pbc[:,0], pbc[:,2])
+		book_prob = get_avg_vector(pbc[:,0], pbc[:,1])
+		pbc[np.where(pbc[:,3]!=0)[0],3] = 1
+		cf_prob = get_cond_prob_vector(pbc[:,0], pbc[:,1], pbc[:,3])
 
-		book_sum = np.bincount(train['prop_id'].values, train['booking_bool'].values)	
-		clck_sum = np.bincount(train['prop_id'].values, train['click_bool'].values)
-		clck_cnt = np.bincount(train['prop_id'].values)
-		clck_prob_all = clck_sum/clck_cnt
-		book_prob_all = book_sum/clck_cnt
+		# Click and Book Probs including dev
+		pbc = np.concatenate((pbc, dev[prop_keys].values))
+		clck_prob_all = get_avg_vector(pbc[:,0], pbc[:,2])
+		book_prob_all = get_avg_vector(pbc[:,0], pbc[:,1])
+		pbc[np.where(pbc[:,3]!=0)[0],3] = 1
+		cf_prob_all = get_cond_prob_vector(pbc[:,0], pbc[:,1], pbc[:,3])
+
 
 		# Account for props found in test but not in train	
 		trdiff = len(px_avg) - len(clck_prob)	
 		if (trdiff > 0):
 			clck_prob = np.lib.pad(clck_prob, (0,trdiff), 'constant', constant_values = (0,0))
 			book_prob = np.lib.pad(book_prob, (0,trdiff), 'constant', constant_values = (0,0))
+			cf_prob   = np.lib.pad(cf_prob, (0,trdiff), 'constant', constant_values = (0,0))
+
 			clck_prob_all = np.lib.pad(clck_prob_all, (0,trdiff), 'constant', constant_values = (0,0))
 			book_prob_all = np.lib.pad(book_prob_all, (0,trdiff), 'constant', constant_values = (0,0))
-			
-	
+			cf_prob_all   = np.lib.pad(cf_prob_all, (0,trdiff), 'constant', constant_values = (0,0))
+				
 		# Save out to cache
 		out_path = data_io.get_paths()["features_cache"]
 		wfp = open(out_path, "w")
 		pickle.dump(px_avg, wfp)
-		pickle.dump(clck_prob, wfp)
-		pickle.dump(book_prob, wfp)
-		pickle.dump(clck_prob_all, wfp)
-		pickle.dump(book_prob_all, wfp)
+		pickle.dump((clck_prob, book_prob, cf_prob), wfp)
+		pickle.dump((clck_prob_all, book_prob_all, cf_prob_all), wfp)
 
-	return px_avg, clck_prob, book_prob, clck_prob_all, book_prob_all
-
+	newdfkeys = ['px_avg', 'clck_prob', 'book_prob', 'cf_prob']
+	newdfvals = [px_avg, clck_prob, book_prob, cf_prob]
+	for setnum, dset in enumerate([train, dev, test]):
+		idx = dset['prop_id'].values
+		if (setnum == 2):
+			newdfvals = [px_avg, clck_prob_all, book_prob_all, cf_prob_all]
+		for dfkey, dfval in zip(newdfkeys, newdfvals):
+			dset[dfkey] = dfval[idx]
+	
+	
 def normalize_price_key(tlist, key, addset=None):
 	px_array = tlist['price_usd'].values
 	val_array = tlist[key].values
@@ -69,9 +88,7 @@ def normalize_price_key(tlist, key, addset=None):
 		px_array = np.concatenate((px_array, addset['price_usd'].values))
 		val_array = np.concatenate((val_array, addset[key].values))
 	px_array[np.where(px_array==0)[0]] = 0.01
-	px_sum = np.bincount(val_array, px_array)
-	px_cnt = np.bincount(val_array)
-	px_avg = np.log10(px_sum/px_cnt)
+	px_avg = np.log10(get_avg_vector(val_array, px_array))
 
 	if ((addset is None)==False):
 		px_array = tlist['price_usd'].values
@@ -79,11 +96,9 @@ def normalize_price_key(tlist, key, addset=None):
 	
 	tlist['norm_px_'+key] = np.log10(px_array) - px_avg[tlist[key].values]
 
-
 def replace_history_features(tlist):
 	tlist['visitor_hist_starrating'] = abs(tlist['visitor_hist_starrating']-tlist['prop_starrating'])	
 	tlist['visitor_hist_adr_usd'] = abs(tlist['visitor_hist_adr_usd']-tlist['price_usd'])
-	return
 
 def save_data_to_ranklib(features, target, RLset):
 	outputlines = []
@@ -100,39 +115,41 @@ def save_data_to_ranklib(features, target, RLset):
 	
 	data_io.write_RLfile(outputlines, RLset)
 
-def augment_data(tlist, px_avg, clck_prob, book_prob, addset=None):
-	tlist['px_avg'] = px_avg[tlist['prop_id'].values]
-	tlist['clck_prob'] = clck_prob[tlist['prop_id'].values]
-	tlist['book_prob'] = book_prob[tlist['prop_id'].values]
 
+def augment_data(tlist, addset=None):
 	replace_history_features(tlist)
-	normalize_price_key(tlist, 'srch_id', addset)
-	normalize_price_key(tlist, 'prop_id', addset)
-	normalize_price_key(tlist, 'srch_destination_id', addset)
+	for setname in ['srch_id', 'prop_id', 'srch_destination_id']:
+		normalize_price_key(tlist, setname, addset)
 				
 def LR_score_to_submission():
 	test = data_io.read_test()
 	score_file = data_io.get_paths()["score_path"]
+	submission_path = data_io.get_paths()["submission_path"]
 	scores = pd.read_csv(score_file, delimiter='\t', header = None)
 	test['rscores'] = scores[2]
 	result = test.sort_index(by=['srch_id','rscores'], ascending=[True, False])
 	output = result[['srch_id','prop_id']]
 	output.columns = ['SearchId', 'PropertyId']	
-	output.to_csv('tmp.csv', index = None)
+	output.to_csv(submission_path, index = None)
 
 
 def dump_train_data():
 	train = data_io.read_train()
 	test = data_io.read_test()
-
+	
+	dev = train[train['srch_id'] % 10 == 1].copy()
+	train_only = train[train['srch_id'] % 10 != 1].copy()
+	
 	# get per Property features over provided data
-	px_avg, clck_prob, book_prob, clck_prob_all, book_prob_all = calc_prop_features(train, test)
+	calc_prop_features(train_only, dev, test)
 	
 	# Supplement data frame with extra features
-	augment_data(train, px_avg, clck_prob, book_prob)
-	augment_data(test,  px_avg, clck_prob_all, book_prob_all, train)
+	
+	augment_data(train_only)
+	for dataset in [dev, test]:
+		augment_data(dataset, train_only)	
 
-	feature_names = list(train.columns)
+	feature_names = list(train_only.columns)
 	feature_names.remove("click_bool")
 	feature_names.remove("booking_bool")
 	feature_names.remove("gross_bookings_usd")
@@ -140,17 +157,18 @@ def dump_train_data():
 	feature_names.remove("position")
 	feature_names = filter(lambda a: a.startswith('comp')==False, feature_names)
 	
-	train.fillna(0, inplace=True)
+	train_only.fillna(0, inplace=True)
+	dev.fillna(0, inplace=True)
 	test.fillna(0, inplace=True)
-	train_target = np.maximum(train['booking_bool'].values*5, train['click_bool'].values)
+	train_target = np.maximum(train_only['booking_bool'].values*5, train_only['click_bool'].values)
+	dev_target = np.maximum(dev['booking_bool'].values*5, dev['click_bool'].values)
 	
-	dev_index = train[train['srch_id'] % 10 == 1].index
 	#downsampled training examples
-	train_index = train[(train['srch_id'] %10 != 1) & ((train['booking_bool'] != 0) | (train['click_bool'] != 0) | (train.index % 3 == 1))].index
-	print "Dev/Train: ", len(dev_index), len(train_index)
+	train_index = train_only[(train_only['booking_bool'] != 0) | (train_only['click_bool'] != 0) | (train_only.index % 3 == 1)].index
+	print "Dev/Train: ", len(dev), len(train_index)
 	
-	save_data_to_ranklib(train.iloc[dev_index][feature_names].values, train_target[dev_index], 'RLdev')
-	save_data_to_ranklib(train.iloc[train_index][feature_names].values, train_target[train_index], 'RLtrain')	
+	save_data_to_ranklib(dev[feature_names].values, dev_target, 'RLdev')
+	save_data_to_ranklib(train_only.loc[train_index][feature_names].values, train_target, 'RLtrain')	
 	save_data_to_ranklib(test[feature_names].values, np.zeros(len(test)), 'RLtest')
 
 def train_model():
